e641b0b13e1e557b1dabe250681a1545948996f1
BMorearty <!security 2&1 /dev/null find-generic-password -gs git.email    | ruby -e 'print $1 if STDIN.gets =~ /^password: \"(.*)\"$/>
2011-04-23 17:00:06 -0700

    Fix tests: remove first "true" param from assertions.
    This parameter was causing all tests to pass whether
    or not they should.

--- test/test_methodfinder.rb
+++ test/test_methodfinder.rb
@@ -5,20 +5,20 @@ class TestMethodFinder < MiniTest::Unit::TestCase
 
   def test_finding_method_with_no_argument_or_block
     result = MethodFinder.find('a', 'A')
-    assert true, result.include?(:capitalize)
-    assert true, result.include?(:upcase)
+    assert result.include?(:capitalize)
+    assert result.include?(:upcase)
   end
 
   def test_finding_method_with_an_argument_and_no_block
     result = MethodFinder.find(10,1,3)
-    assert true, result.include?(:%)
-    assert true, result.include?(:modulo)
+    assert result.include?(:%)
+    assert result.include?(:modulo)
   end
 
   def test_finding_method_with_a_block
     result = MethodFinder.find(%w(a b), %w(A B)) { |x| x.upcase }
-    assert true, result.include?(:map)
-    assert true, result.include?(:collect)
+    assert result.include?(:map)
+    assert result.include?(:collect)
   end
 
   def test_block_interface
@@ -28,18 +28,18 @@ class TestMethodFinder < MiniTest::Unit::TestCase
 
   def test_instance_interface
     result = 'a'.find_method 'A'
-    assert true, result.include?(:capitalize)
-    assert true, result.include?(:upcase)
+    assert result.include?(:capitalize)
+    assert result.include?(:upcase)
   end
 
   def test_instance_interface_with_params
     result = %w[a b c].find_method %w[a b], %w[c]
-    assert true, result.include?(:-)
+    assert result.include?(:-)
   end
 
   def test_find_classes_and_modules
-    assert true, MethodFinder.find_classes_and_modules.include?(Array)
-    assert true, MethodFinder.find_classes_and_modules.include?(Math)
+    assert MethodFinder.find_classes_and_modules.include?(Array)
+    assert MethodFinder.find_classes_and_modules.include?(Math)
   end
 
   def test_find_in_class_or_module
@@ -54,18 +54,17 @@ class TestMethodFinder < MiniTest::Unit::TestCase
       MethodFinder.find_in_class_or_module(c, pattern)
     end
 
-    assert true,
-    MethodFinder.find_in_class_or_module(Math, /sin/).include?(:sin)
+    assert MethodFinder.find_in_class_or_module(Math, /sin/).include?(:sin)
 
     result = MethodFinder.find_in_class_or_module(:Float, /^to/)
-    assert true, result.include?(:to_f)
-    assert true, result.include?(:to_r)
+    assert result.include?(:to_f)
+    assert result.include?(:to_r)
 
-    assert true, MethodFinder.find_in_class_or_module(Array).size > 10
+    assert MethodFinder.find_in_class_or_module(Array).size > 10
   end
 
   def test_ignores_items_in_blacklist
     MethodFinder::INSTANCE_METHOD_BLACKLIST[:Object] << :flatten
-    assert true, !MethodFinder.find([2,3,4], [2,3,4]).include?(:flatten)
+    assert !MethodFinder.find([2,3,4], [2,3,4]).include?(:flatten)
   end
 end
