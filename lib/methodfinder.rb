b87af1f1ed7b2eaaf9e893dc1e213250ff98f953
Michael Kohl <citizen428@gmail.com>
2018-04-30 18:47:16 +0800

    Clean up code

--- lib/methodfinder.rb
+++ lib/methodfinder.rb
@@ -8,7 +8,7 @@ class Object
   #    %w[a b c].find_method { |a| a.unknown(1) ; a == %w[a c] }
   #    #=> ["Array#delete_at", "Array#slice!"]
   #    10.find_method { |n| n.unknown(3) == 1 }
-  #    #=> ["Fixnum#%", "Fixnum#<=>", "Fixnum#>>", "Fixnum#[]", "Integer#gcd", "Fixnum#modulo", "Numeric#remainder"]
+  #    #=> ["Fixnum#%", "Fixnum#<=>", "Fixnum#>>", ...]
   #
   # Inside <tt>find_method</tt>'s block, the receiver is available as
   # block argument and the special method <tt>unknown</tt> is used as
@@ -17,21 +17,18 @@ class Object
   # <tt>find_method</tt> can be called without passing a block. This
   # is the same as calling <tt>MethodFinder.find</tt>.
   #
-  #    10.find_method(1,3)
-  #    #=> ["Fixnum#%", "Fixnum#<=>", "Fixnum#>>", "Fixnum#[]", "Integer#gcd", "Fixnum#modulo", "Numeric#remainder"]
+  #    10.find_method(1, 3)
+  #    #=> ["Fixnum#%", "Fixnum#<=>", "Fixnum#>>", ...]
+  def find_method(*args, debug: ENV['METHOD_FINDER_DEBUG'])
+    return MethodFinder.find(self, *args, debug: debug) unless block_given?
 
-  def find_method(*args, debug: ENV['METHOD_FINDER_DEBUG'], &block)
-    if block
-      mets = MethodFinder.methods_to_try(self).select do |met|
-        STDERR.puts met if debug
-        self.class.class_eval("alias :unknown #{met}")
-        obj = self.dup rescue self # dup doesn't work for immutable types
-        block.call(obj) rescue nil
-      end
-      mets.map { |m| "#{self.method(m).owner}##{m}" }
-    else
-      MethodFinder.find(self, *args, debug: debug)
+    found = MethodFinder.methods_to_try(self).select do |met|
+      STDERR.puts(met) if debug
+      self.class.class_eval("alias :unknown #{met}", __FILE__, __LINE__)
+      obj = self.dup rescue self # dup doesn't work for immutable types
+      yield(obj) rescue nil
     end
+    found.map { |m| "#{method(m).owner}##{m}" }
   end
 end
 
@@ -39,7 +36,7 @@ module MethodFinder
   # Default arguments for methods
   ARGS = {
     cycle: [1] # prevent cycling forever
-  }
+  }.freeze
 
   # Blacklisting methods, e.g. { :Object => [:ri, :vim] }
   INSTANCE_METHOD_BLACKLIST = Hash.new { |h, k| h[k] = [] }
@@ -53,89 +50,93 @@ module MethodFinder
     CLASS_METHOD_BLACKLIST[:Object] << :pry
   end
 
-    # Provided with a receiver, the desired result and possibly some
-    # arguments, <tt>MethodFinder.find</tt> will list all methods that
-    # produce the given result when called on the receiver with the
-    # provided arguments.
-    #
-    #    MethodFinder.find(10, 1, 3)
-    #    #=> ["Fixnum#%", "Fixnum#<=>", "Fixnum#>>", "Fixnum#[]", "Integer#gcd", "Fixnum#modulo", "Numeric#remainder"]
-    #    MethodFinder.find("abc","ABC")
-    #    #=> ["String#swapcase", "String#swapcase!", "String#upcase", "String#upcase!"]
-    #    MethodFinder.find(10, 100, 2)
-    #    #=> ["Fixnum#**"]
-    #    MethodFinder.find(['a','b','c'], ['A','B','C']) { |x| x.upcase }
-    #    #=> ["Array#collect", "Array#collect!", "Enumerable#collect_concat", "Enumerable#flat_map", "Array#map", "Array#map!"]
+  # Provided with a receiver, the desired result and possibly some
+  # arguments, <tt>MethodFinder.find</tt> will list all methods that
+  # produce the given result when called on the receiver with the
+  # provided arguments.
+  #
+  #    MethodFinder.find(10, 1, 3)
+  #    #=> ["Fixnum#%", "Fixnum#<=>", "Fixnum#>>", "Fixnum#[]", ...]
+  #    MethodFinder.find("abc","ABC")
+  #    #=> ["String#swapcase", "String#swapcase!", "String#upcase", ...]
+  #    MethodFinder.find(10, 100, 2)
+  #    #=> ["Fixnum#**"]
+  #    MethodFinder.find(['a','b','c'], ['A','B','C']) { |x| x.upcase }
+  #    #=> ["Array#collect", "Array#collect!", "Enumerable#collect_concat", ...]
+  def self.find(obj, res, *args, debug: ENV['METHOD_FINDER_DEBUG'], &block)
+    redirect_streams
 
-    def self.find(obj, res, *args, debug: ENV['METHOD_FINDER_DEBUG'], &block)
-      redirect_streams
-
-      mets = methods_to_try(obj).select do |met|
-        o = obj.dup rescue obj
-        m = o.method(met)
-        if m.arity <= args.size
-          STDERR.puts met if debug
-          a = args.empty? && ARGS.has_key?(met) ? ARGS[met] : args
-          m.call(*a, &block) == res rescue nil
-        end
-      end
-      mets.map { |m| "#{obj.method(m).owner}##{m}" }
-    ensure
-      restore_streams
+    found = methods_to_try(obj).select do |met|
+      o = obj.dup rescue obj
+      m = o.method(met)
+      next unless m.arity <= args.size
+      STDERR.puts(met) if debug
+      a = args.empty? && ARGS.key?(met) ? ARGS[met] : args
+      m.call(*a, &block) == res rescue nil
     end
+    found.map { |m| "#{obj.method(m).owner}##{m}" }
+  ensure
+    restore_streams
+  end
 
-    # Returns all currently defined modules and classes.
-    def self.find_classes_and_modules
-      constants = Object.constants.sort.map { |c| Object.const_get(c) }
-      constants.select { |c| c.class == Class || c.class == Module}
-    end
+  # Returns all currently defined modules and classes.
+  def self.find_classes_and_modules
+    constants = Object.constants.sort.map { |c| Object.const_get(c) }
+    constants.select { |c| c.class == Class || c.class == Module }
+  end
 
-    # Searches for a given name within a class. The first parameter
-    # can either be a class object, a symbol or a string whereas the
-    # optional second parameter can be a string or a regular
-    # expression:
-    #
-    #    MethodFinder.find_in_class_or_module('Array', 'shuff')
-    #    #=> [:shuffle, :shuffle!]
-    #    MethodFinder.find_in_class_or_module(Float, /^to/)
-    #    #=> [:to_f, :to_i, :to_int, :to_r, :to_s]
-    #
-    # If the second parameter is omitted, all methods of the class or
-    # module will be returned.
-    #
-    #    MethodFinder.find_in_class_or_module(Math)
-    #    #=> [:acos, :acosh, :asin ... :tanh]
+  # Searches for a given name within a class. The first parameter
+  # can either be a class object, a symbol or a string whereas the
+  # optional second parameter can be a string or a regular
+  # expression:
+  #
+  #    MethodFinder.find_in_class_or_module('Array', 'shuff')
+  #    #=> [:shuffle, :shuffle!]
+  #    MethodFinder.find_in_class_or_module(Float, /^to/)
+  #    #=> [:to_f, :to_i, :to_int, :to_r, :to_s]
+  #
+  # If the second parameter is omitted, all methods of the class or
+  # module will be returned.
+  #
+  #    MethodFinder.find_in_class_or_module(Math)
+  #    #=> [:acos, :acosh, :asin ... :tanh]
+  def self.find_in_class_or_module(klass, pattern = /./)
+    klasses = Object.const_get(klass.to_s)
+    class_methods = klasses.methods(false) rescue []
+    instance_methods = klasses.instance_methods(false)
+    all_methods = class_methods + instance_methods
+    all_methods.grep(/#{pattern}/).sort
+  end
 
-    def self.find_in_class_or_module(c, pattern=/./)
-      cs = Object.const_get(c.to_s)
-      class_methods = cs.methods(false) rescue []
-      instance_methods = cs.instance_methods(false)
-      all_methods = class_methods + instance_methods
-      all_methods.grep(/#{pattern}/).sort
-    end
+  # Returns a list of candidate methods for a given object. Added by Jan Lelis.
+  def self.methods_to_try(obj)
+    ret = obj.methods
+    blacklist = select_blacklist(obj)
+    klass = obj.is_a?(Module) ? obj : obj.class
 
-    # Returns a list of candidate methods for a given object. Added by Jan Lelis.
-    def self.methods_to_try(obj)
-      ret = obj.methods
-      blacklist = obj.is_a?(Module) ? CLASS_METHOD_BLACKLIST : INSTANCE_METHOD_BLACKLIST
-      klass = obj.is_a?(Module) ? obj : obj.class
+    klass.ancestors.each { |ancestor| ret -= blacklist[ancestor.to_s.intern] }
+    ret.sort
+  end
 
-      klass.ancestors.each { |ancestor| ret -= blacklist[ancestor.to_s.intern] }
+  # :nodoc:
+  def self.redirect_streams
+    @orig_stdout = $stdout
+    @orig_stderr = $stderr
+    $stdout = StringIO.new
+    $stderr = StringIO.new
+  end
+  private_class_method :redirect_streams
 
-      ret.sort
-    end
+  # :nodoc:
+  def self.restore_streams
+    $stdout = @orig_stdout
+    $stderr = @orig_stderr
+  end
+  private_class_method :restore_streams
 
-    # :nodoc:
-    def self.redirect_streams
-      @orig_stdout = $stdout
-      @orig_stderr = $stderr
-      $stdout = StringIO.new
-      $stderr = StringIO.new
-    end
-
-    def self.restore_streams
-      $stdout = @orig_stdout
-      $stderr = @orig_stderr
-    end
-  private_class_method :redirect_streams, :restore_streams
+  # :nodoc:
+  def self.select_blacklist(object)
+    object.is_a?(Module) ? CLASS_METHOD_BLACKLIST : INSTANCE_METHOD_BLACKLIST
+  end
+  private_class_method :select_blacklist
 end
